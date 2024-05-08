81c22013e552789e58f5fd48176f3aee51362fa8
Xanion11 <krzysztof.j.ciesla@gmail.com>
2024-05-08 21:27:06 +0200

    asd

--- init.lua
+++ init.lua
@@ -1,89 +1,3 @@
---[[
-
-=====================================================================
-==================== READ THIS BEFORE CONTINUING ====================
-=====================================================================
-========                                    .-----.          ========
-========         .----------------------.   | === |          ========
-========         |.-""""""""""""""""""-.|   |-----|          ========
-========         ||                    ||   | === |          ========
-========         ||   KICKSTART.NVIM   ||   |-----|          ========
-========         ||                    ||   | === |          ========
-========         ||                    ||   |-----|          ========
-========         ||:Tutor              ||   |:::::|          ========
-========         |'-..................-'|   |____o|          ========
-========         `"")----------------(""`   ___________      ========
-========        /::::::::::|  |::::::::::\  \ no mouse \     ========
-========       /:::========|  |==hjkl==:::\  \ required \    ========
-========      '""""""""""""'  '""""""""""""'  '""""""""""'   ========
-========                                                     ========
-=====================================================================
-=====================================================================
-
-What is Kickstart?
-
-  Kickstart.nvim is *not* a distribution.
-
-  Kickstart.nvim is a starting point for your own configuration.
-    The goal is that you can read every line of code, top-to-bottom, understand
-    what your configuration is doing, and modify it to suit your needs.
-
-    Once you've done that, you can start exploring, configuring and tinkering to
-    make Neovim your own! That might mean leaving Kickstart just the way it is for a while
-    or immediately breaking it into modular pieces. It's up to you!
-
-    If you don't know anything about Lua, I recommend taking some time to read through
-    a guide. One possible example which will only take 10-15 minutes:
-      - https://learnxinyminutes.com/docs/lua/
-
-    After understanding a bit more about Lua, you can use `:help lua-guide` as a
-    reference for how Neovim integrates Lua.
-    - :help lua-guide
-    - (or HTML version): https://neovim.io/doc/user/lua-guide.html
-
-Kickstart Guide:
-
-  TODO: The very first thing you should do is to run the command `:Tutor` in Neovim.
-
-    If you don't know what this means, type the following:
-      - <escape key>
-      - :
-      - Tutor
-      - <enter key>
-
-    (If you already know the Neovim basics, you can skip this step.)
-
-  Once you've completed that, you can continue working through **AND READING** the rest
-  of the kickstart init.lua.
-
-  Next, run AND READ `:help`.
-    This will open up a help window with some basic information
-    about reading, navigating and searching the builtin help documentation.
-
-    This should be the first place you go to look when you're stuck or confused
-    with something. It's one of my favorite Neovim features.
-
-    MOST IMPORTANTLY, we provide a keymap "<space>sh" to [s]earch the [h]elp documentation,
-    which is very useful when you're not exactly sure of what you're looking for.
-
-  I have left several `:help X` comments throughout the init.lua
-    These are hints about where to find more information about the relevant settings,
-    plugins or Neovim features used in Kickstart.
-
-   NOTE: Look for lines like this
-
-    Throughout the file. These are for you, the reader, to help you understand what is happening.
-    Feel free to delete them once you know what you're doing, but they should serve as a guide
-    for when you are first encountering a few different constructs in your Neovim config.
-
-If you experience any errors while trying to install kickstart, run `:checkhealth` for more info.
-
-I hope you enjoy your Neovim journey,
-- TJ
-
-P.S. You can delete this when you're done too. It's your config now! :)
---]]
-
 -- Set <space> as the leader key
 -- See `:help mapleader`
 --  NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used)
@@ -103,7 +17,8 @@ vim.opt.number = true
 -- You can also add relative line numbers, to help with jumping.
 --  Experiment for yourself to see if you like it!
 vim.opt.relativenumber = true
-
+vim.opt.shiftwidth = 4
+vim.opt.tabstop = 4
 -- Enable mouse mode, can be useful for resizing splits for example!
 vim.opt.mouse = 'a'
 
@@ -156,7 +71,10 @@ vim.opt.scrolloff = 10
 
 -- [[ Basic Keymaps ]]
 --  See `:help vim.keymap.set()`
-
+vim.keymap.set('n', 'H', '<CMD>bprevioues<CR>')
+vim.keymap.set('n', 'V', '<CMD>windo wincmd H<CR>')
+vim.keymap.set('n', 'B', '<CMD>windo wincmd K<CR>')
+vim.keymap.set('n', 'L', '<CMD>bnext<CR>')
 -- Set highlight on search, but clear on pressing <Esc> in normal mode
 vim.opt.hlsearch = true
 vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
@@ -350,11 +268,11 @@ require('lazy').setup({
         -- You can put your default mappings / updates / etc. in here
         --  All the info you're looking for is in `:help telescope.setup()`
         --
-        -- defaults = {
-        --   mappings = {
-        --     i = { ['<c-enter>'] = 'to_fuzzy_refine' },
-        --   },
-        -- },
+        defaults = {
+          mappings = {
+            i = { ['<c-enter>'] = 'to_fuzzy_refine' },
+          },
+        },
         -- pickers = {}
         extensions = {
           ['ui-select'] = {
@@ -385,7 +303,7 @@ require('lazy').setup({
         -- You can pass additional configuration to Telescope to change the theme, layout, etc.
         builtin.current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
           winblend = 10,
-          previewer = false,
+          previewer = true,
         })
       end, { desc = '[/] Fuzzily search in current buffer' })
 
@@ -419,6 +337,89 @@ require('lazy').setup({
         vim.keymap.set('n', '<C-.>', '<CMD>lua require("harpoon.ui").nav_next()<CR>'),
       },
     },
+  },
+
+  {
+    'nvimdev/dashboard-nvim',
+    event = 'VimEnter',
+    config = function()
+      require('dashboard').setup {
+        theme = 'hyper',
+        --  config = {
+        --    week_header = {
+        -- enable = true,
+
+        config = {
+          header = {
+
+            '                                                                                                                           ██               ██                       ',
+            '                                                                                                                            ██               ██                       ',
+            '                                                                                                                            ██             ████                       ',
+            '██                               ██████████                                                                               ████             ████                       ',
+            '██                       ██████████████████                                                                               █████         █████████                     ',
+            '████                    ███████████                                                                                       █████         █████████                     ',
+            '█████                  ████████████                                                                                      ███████       ███████████                     ',
+            '███████              ███████████                                                                             ██          █████████    ████████████                     ',
+            '███████             ██████████                                                                               ██         ██████████    ████████████                     ',
+            '███████          █████████████                                                                             ██████     ████████████████████████████                     ',
+            '███████          ███████████                                                                              ███████     ██████████████████████████                       ',
+            '███████          ██████████                                                                             █████████     ████████████████████████                         ',
+            '██████████       ██████████                                                                    ███        ███████       ███████████████████████                          ',
+            '██████████      ███████████       ██████████                                                   ███        ███████       ███████████████████████                          ',
+            '██████████      █████████████████████████████████ ████████████████      ████                   ███        ███████       █████████████████████                            ',
+            '████████████    ██████████ ███████████████████████████████████████████    ████                   ███                      █████████████████████                            ',
+            '████████████    ██████████ ███████████████████ ████████████████████████   ████                  ████        ███          ██████████████████████                            ',
+            '█████████████████████████████████████       █████████████████████████████ █████                 ████       ████          ██████████████████████                            ',
+            '████████████████████████ ██████████          █████████████████████████████ ███████            ███████       ████          ██████████  █████████                             ',
+            '████████████████████████████████████████    ███████████         ███████████████████         █████████     ██████        ████████████ ██████████                             ',
+            '████████████████████████ ████████████████████ ████████               ████████████████       ███████████    ███████        █████████    ████████                               ',
+            '██████████ ███████████████████████████████████ ████████                 ████████████████     ███████████    ███████       ██████████    ████████                               ',
+            '████████   █████████████ ███████████           ███████                  ██████████████████  ████████████  █████████     ████████████     █████████                             ',
+            '████████   █████████████ ████████              ███████                  ████████████████████████████████  █████████   ██████████████     ██████████                            ',
+            '█████████    ███████████   ████████    ███████   ████████               ██████████████████████████████      ███████    █████████████       ██████████                            ',
+            '██████████    ███████████   █████████████████████ ██████████           ███████████  ███████████████████    █████████ ███████████████        ██████████                            ',
+            '████████      ██████████    █████████████████████ ████████████████████████████████  ███████████████████    █████████████████████████          ███████████                         ',
+            '█████████        █████████    ███████████████████    █████████████████████████████       ███████████████     ███████ █████████████              ███████████                         ',
+            '█████████        ███████       ██████████████████      █████████████████████████         █████████████       ███████ ████████████               ███████████                         ',
+            '███████          ███████                                     ███████████████             ██████████          ██████████████████                  ███████████████                    ',
+            '███████          ██████                                                                    ████████          ████ ████████████                     █████████████                    ',
+            '█████              ████                                                                     ███████          ████ █████████                        ████████████████                 ',
+            '████               ██                                                                         ████             █████████                              ███████████████               ',
+            '██                 ██                                                                                            ███████                                █████████████               ',
+            '██                                                                                                               █████                                        ██████████            ',
+            '                                                                                                                 ███                                           █████████            ',
+            '                                                                                                                 ███                                               ███████          ',
+            '                                                                                                                 ██                                                   ████          ',
+            '                                                                                                                                                                    ████',
+            ' ',
+          },
+          shortcut = {
+            { desc = '󰊳 Update', group = '@property', action = 'Lazy update', key = 'u' },
+            {
+              icon = ' ',
+              icon_hl = '@variable',
+              desc = 'Files',
+              group = 'Label',
+              action = 'Telescope find_files',
+              key = 'f',
+            },
+            {
+              desc = ' Open config',
+              group = 'Label',
+              action = 'e $MYVIMRC',
+              key = 'a',
+            },
+            {
+              desc = ' LSP',
+              group = 'Number',
+              action = 'Mason',
+              key = 'd',
+            },
+          },
+        },
+      }
+    end,
+    dependencies = { { 'nvim-tree/nvim-web-devicons' } },
   },
   { -- LSP Configuration & Plugins
     'neovim/nvim-lspconfig',
@@ -803,7 +804,12 @@ require('lazy').setup({
       local statusline = require 'mini.statusline'
       -- set use_icons to true if you have a Nerd Font
       statusline.setup { use_icons = vim.g.have_nerd_font }
-
+      --
+      -- local starter = require 'mini.starter'
+      --
+      -- starter.setup {
+      --   header = '⣿⣿⣿⡿⢿⡿⢿⡿⣟⡿⡏⢿⡿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡆⢸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿\n⣿⣿⣻⣿⡟⣿⣙⣭⣟⣯⢿⡈⣿⣽⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇⢸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿\n⣿⣿⣿⢯⣿⡏⢿⣻⣾⣻⣯⢳⠸⣯⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⢆⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿\n⣿⣿⣿⣟⣷⣽⣋⣼⡿⣽⣾⣛⡆⢟⣿⣷⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿\n⣿⣿⣿⣿⣿⣿⣿⡿⢿⣳⣯⡟⣇⢾⣽⣻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠿⠟⢱⣿⣿⣿⣿⣿⡜⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿\n⣿⣿⣿⣿⣿⣿⣿⣟⣻⣹⢷⣻⡽⣎⠻⣿⣿⣿⣿⠿⠛⠋⠉⠉⠄⠐⠤⣀⠉⠉⠉⠙⠛⠛⢿⠿⢏⣭⣄⣦⣶⣿⣿⣿⣿⣿⣿⢧⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿\n⣿⣿⣿⣿⣿⡿⣟⣾⢿⡭⣟⣿⣯⠭⣑⢸⣿⣿⠈⡤⠖⠒⠒⠂⠀⠉⠑⠂⠀⠀⠀⠀⠤⠍⠤⠴⢋⠹⣿⣿⣿⣿⣿⣿⣿⠟⠋⣈⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿\n⣿⣿⣿⣿⢿⣹⣿⣉⢷⣹⣿⢿⢱⢇⡏⣸⣿⠀⣷⡈⠶⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣰⡎⠶⣿⣿⣿⣿⣿⡿⠁⢀⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿\n⣿⢿⡽⣞⡯⣟⠶⣭⣳⡿⢫⢎⠣⢯⢰⣻⠃⢰⣿⠉⠳⡌⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠁⣠⡼⣡⠻⣿⢿⣿⡟⠁⢠⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿\n⡯⢯⡝⣧⢻⡜⡳⡔⠲⣍⠧⢫⠜⡞⣸⢹⡴⢿⣿⣷⣾⠃⣧⣤⣄⠀⠀⠀⢀⣀⣀⣤⣤⣤⣤⣅⣷⡙⡖⣿⢏⡏⠀⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿\n⢓⠢⢉⠶⣹⢞⡵⣛⣄⠣⠚⣅⡾⢡⣧⣾⠃⠸⣿⡯⠇⠐⠛⢿⣿⡇⠀⠀⢘⣿⣿⠯⠤⠤⠭⢛⡀⢻⣘⣿⣾⠇⢀⡝⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿\n⠈⢄⠻⡜⣧⢻⡜⠶⡜⠬⢵⣼⠀⢎⣿⡟⢀⡾⠋⠭⠔⠀⠂⢈⣿⠃⠀⠀⠈⠉⠳⠖⣒⠒⠋⠑⠈⠲⢍⠳⠞⠉⢽⣿⣮⢻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿\n⠀⠀⠁⠙⢀⠳⡈⢳⡘⢂⣿⠜⡆⠈⣿⡇⣸⡇⠀⠀⠀⠀⠀⣾⡿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢊⣿⣧⠀⣾⣿⣿⣷⢻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿\n⠀⠀⠀⠀⠀⢈⠠⠁⣼⡿⢣⠆⠑⢄⢹⡇⠀⣷⠀⠀⠀⠀⢰⣿⠁⠀⠀⠀⠀⣄⠀⠀⠀⠀⠀⠀⠀⢀⡚⡿⢈⠅⣿⣿⣿⣿⣏⠿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿\n⠀⠀⠀⠀⠀⠀⠄⠣⠌⢩⠃⠊⠐⠀⢸⣷⠀⣿⣆⠀⠀⠀⢾⣿⣷⣤⣰⢶⣶⡼⠀⠀⠀⠀⠀⠀⠀⠠⢱⡗⠋⣼⣿⣿⣿⣿⣿⣷⣭⡛⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿\n⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠸⡔⡇⢻⡄⣀⠀⠀⢉⣻⡏⣥⡀⠉⠀⠀⠀⠀⠀⠀⠀⢀⠠⢹⣧⣴⣧⠻⣿⣿⣿⣿⣿⣿⣾⢹⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿\n⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡇⡑⠘⡿⣷⣴⣾⣛⠫⡀⢉⠙⠻⠦⣄⣀⠀⠀⢀⢀⠂⢤⣿⣿⣿⣿⣽⠹⣿⣿⣿⣿⣿⣿⣯⠻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿\n⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠸⣇⡀⠀⣿⣿⡛⠛⠓⠟⠛⠓⠞⢷⠚⣻⠃⠀⠂⠄⣢⣾⢿⣿⣿⣿⢿⣹⠾⢿⣿⣿⣭⢳⣿⣟⣾⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿\n⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠹⣷⠀⣿⣻⣧⣄⣀⣀⣠⣤⡶⠋⠀⠘⡒⠌⣐⣾⣿⠟⣻⣿⣿⣿⢻⣜⡢⠍⠻⣿⣾⣧⡟⣿⣎⢳⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿\n⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠹⣧⢹⣿⡿⡍⠋⠙⠋⠁⠀⠈⠀⢀⢠⣼⣾⣿⠃⡘⢜⡻⣷⣷⣿⣿⣿⣦⢀⠘⠇⠈⢹⣿⣿⣝⣷⣽⡻⣿⣿⣿⣿⣿⣿⣿⣿\n⠀⠀⠀⠀⠀⡜⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⣷⣽⣷⣦⣁⠀⠀⢀⣀⣀⣤⣾⣿⣿⠟⠠⠀⠐⢌⡱⠛⠻⣿⣿⣿⣿⣦⢣⡀⠀⠘⢿⢻⣯⣞⣫⣽⡙⣿⣿⣿⣿⣿⣿⣿\n⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠖⣻⣛⠿⠶⠀⣹⣿⣿⣿⣿⣿⣿⣶⣿⣿⣿⣿⡟⢁⠊⠀⠀⠐⠨⠄⠁⠁⠈⠉⢻⣿⣿⣨⠿⣄⡀⠀⠀⢝⣾⣿⣿⣷⡽⣿⣿⣿⣿⣿⣿\n⡹⣆⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢰⡾⣱⣏⣿⣷⣾⣿⡿⣿⣿⣿⣿⣿⣿⣿⣿⡿⢱⠰⠆⠀⠀⠀⠀⠀⠀⠀⠀⠀⣰⣿⣿⣿⣿⣶⠁⢱⡀⠀⠈⠎⢿⣿⣿⢷⣹⣿⣿⣿⣿⣿\n⠳⡄⠃⠀⠀⢀⣀⠀⠀⠀⠀⡀⢀⣷⣿⣿⣿⣿⣿⣿⣿⣿⢸⣿⣿⣿⠿⣻⢟⠻⣑⠣⠒⠀⠀⠀⠀⠀⠀⠀⠀⣠⣾⣿⣿⣿⣿⣿⣿⣷⣼⡹⡀⠀⠀⠂⠋⣕⠎⡍⡿⢿⣻⣟⣿\n⡽⣌⠣⠁⢰⡳⢉⠶⣡⢎⣵⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣄⢻⣟⣣⢓⡐⢊⡑⢀⠂⠁⡐⠀⠀⠀⠀⣠⣴⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣦⣈⢀⠸⡜⣎⢭⡰⣝⢿⢿⣿⣿\n⣗⢎⣱⢂⡼⣐⣩⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠟⢛⣺⡓⠷⣎⡄⠃⠀⠀⠀⠐⠀⣀⣤⣶⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⢛⣯⡛⢿⣿⣿⣦⡙⠹⣿⣷⡭⢇⠈⡑⢯\n⣿⣫⢷⢺⣡⣾⣛⣽⣿⣿⣿⣿⣿⣿⣿⡿⢟⣫⣿⣾⣾⣿⣿⣿⣿⣵⣶⣌⣉⣓⡦⢴⣶⣿⣿⣿⣿⣿⣿⣿⣿⣿⠿⠿⠿⢟⣫⣵⣾⣿⣿⣿⣷⣶⣶⣶⣾⣍⣛⠫⠜⠆⠀⠈⠒\n⣿⡿⣏⣗⡚⠛⢿⣿⣿⠿⠿⠿⠿⣛⣷⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣮⣛⠿⣿⡿⠿⠛⣉⣤⣶⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣿⣿⡗⢀⠀⠀⠀\n⣿⣿⣿⣿⣿⣿⣶⣷⣶⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣶⣶⣶⣿⣿⣿⣿⣿⠿⠋⠉⢹⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣎⠛⠇⠀\n⣿⣿⣿⣿⣿⣿⣿⣿⣿⠟⣫⣍⣛⣛⡛⠿⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡆⠀⣀⣹⣯⣽⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷\n',
+      -- }
       -- You can configure sections in the statusline by overriding their
       -- default behavior. For example, here we set the section for
       -- cursor location to LINE:COLUMN
@@ -820,7 +826,7 @@ require('lazy').setup({
     'nvim-treesitter/nvim-treesitter',
     build = ':TSUpdate',
     opts = {
-      ensure_installed = { 'bash', 'c', 'html', 'lua', 'luadoc', 'markdown', 'vim', 'vimdoc' },
+      ensure_installed = { 'bash', 'c', 'html', 'lua', 'luadoc', 'markdown', 'vim', 'vimdoc', 'rust' },
       -- Autoinstall languages that are not installed
       auto_install = true,
       highlight = {
